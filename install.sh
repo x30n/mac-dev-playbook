ebdb966f333f84c9074755a549f38a61097ee181
Karl Hepworth <karl.hepworth@gmail.com>
2016-10-20 11:12:55 +1100

    Automate the setup a little more...

--- install.sh
+++ install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
-if [[ -z $(which ansible) ]]; then
-    echo "Install PIP";
-    sudo easy_install pip;
-    echo "Install Ansible";
-    sudo pip install ansible;
-    echo "Make Ansible directory and set permissions.";
-    sudo mkdir -p /etc/ansible
-    sudo chown -R $(whoami) /etc/ansible
-    echo "Make Brew directory and set permissions.";
-    sudo mkdir -p /usr/local
-    sudo chown -R $(whoami) /etc/ansible
+if [[ -z $(which brew) ]]; then
+  echo "Installing Homebrew...";
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null;
 fi
 
-ansible-galaxy install -r requirements.yml
-ansible-playbook main.yml -i inventory -U $(whoami) --ask-sudo-pass
+if [[ -z $(which ansible) ]]; then
+    echo "Installing Ansible";
+    brew install ansible > /dev/null;
+fi
+
+if [[ -d "~/Documents/dotfiles" ]]; then
+    echo "Removing dotfiles";
+    rm -rf ~/Documents/dotfiles > /dev/null;
+fi
+
+if [[ -d "~/.setup" ]]; then
+    echo "Removing playbook";
+    rm -rf ~/.setup > /dev/null;
+fi
+
+git clone git@github.com:fubarhouse/mac-dev-playbook.git ~/.setup > /dev/null;
+git clone git@github.com:fubarhouse/mac-dev-playbook-dotfiles.git ~/Documents/dotfiles > /dev/null;
+
+ansible-galaxy install -r requirements.yml > /dev/null;
+ansible-playbook main.yml -i inventory -U $(whoami) --ask-sudo-pass > /dev/null;
+
+if [[ -d "~/Documents/dotfiles" ]]; then
+    echo "Removing dotfiles";
+    rm -rf ~/Documents/dotfiles  > /dev/null;
+fi
+
+if [[ -d "~/.setup" ]]; then
+    echo "Removing playbook";
+    rm -rf ~/.setup > /dev/null;
+fi
