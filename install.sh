8c5770a0f3c3862137b0e9402c8ba511a02b65d2
Karl Hepworth <karl.hepworth@gmail.com>
2016-10-24 15:17:33 +1100

    add debugger

--- install.sh
+++ install.sh
@@ -40,6 +40,7 @@ ansible-galaxy install -r ./requirements.yml;
 
 echo "Initiating playbook";
 
+echo "ansible-playbook ./main.yml -i inventory -U $(whoami) --ask-sudo-pass --extra-vars "\"${EXTRAVARS}\"";"
 ansible-playbook ./main.yml -i inventory -U $(whoami) --ask-sudo-pass --extra-vars "\"${EXTRAVARS}\"";
 
 echo "Done.";
